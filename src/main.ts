9c68ece1f9622fd0f6f64099ee39cc535be355df
Henry Zhang <henry.zhang@later.com>
2022-09-13 15:29:09 -0700

    (main.ts): get account data

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import got from 'got';
+import { JSDOM } from 'jsdom';
 
 const BASE_URI = 'https://www.linktr.ee/'
 
 export default async function scraper(profile: string): Promise<any> {
   const response = await got(BASE_URI + profile);
-  console.log('file: main.ts 😈😈😈 line 7 😈😈😈 scraper 😈😈😈 response', response)
+  const document = new JSDOM(response.body).window.document;
+
+  const data = document.querySelectorAll('#__NEXT_DATA__')
+  const accountData = JSON.parse(data[0].innerHTML)
+  console.log('file: main.ts 😈😈😈 line 16 😈😈😈 scraper 😈😈😈 accountData', accountData['props']['pageProps']['account'])
 
 }
 
