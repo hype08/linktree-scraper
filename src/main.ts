9ca3235f296f3e0ee6b7c601d1b2cfa5431df553
Henry Zhang <henry.zhang@later.com>
2022-09-13 15:33:07 -0700

    (main.ts): cleanup constants

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,10 @@ export default async function scraper(profile: string): Promise<any> {
   const response = await got(BASE_URI + profile);
   const document = new JSDOM(response.body).window.document;
 
-  const data = document.querySelectorAll('#__NEXT_DATA__')
-  const accountData = JSON.parse(data[0].innerHTML)
-  console.log('file: main.ts 😈😈😈 line 16 😈😈😈 scraper 😈😈😈 accountData', accountData['props']['pageProps']['account'])
+  const dataNode = document.querySelectorAll('#__NEXT_DATA__')
+  const linkTreeData = JSON.parse(dataNode[0].innerHTML)
+
+  const accountData = linkTreeData['props']['pageProps']['account']
 
 }
 
